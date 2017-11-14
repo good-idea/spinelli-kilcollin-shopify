8d8470f8519951702080193180cd0685c2414ae8
Joseph Thomas <info@standard-quality.biz>
2017-11-14 15:04:54 -0800

    Adds lazy loading, smarter srcsets

--- src/js/components/imageLoad.js
+++ src/js/components/imageLoad.js
@@ -1,6 +1,8 @@
 import $ from 'npm-zepto';
 import publisher from '../tools/pubSub';
 
+const windo = $(window)
+
 function watchImage(element, onLoadCallback) {
 	const mainContainer = (element instanceof $) ? element : $(element);
 	const fullImage = mainContainer.find('.img-preload-full img');
@@ -9,16 +11,43 @@ function watchImage(element, onLoadCallback) {
 		onLoadCallback(mainContainer);
 	});
 
+	let imageTop;
+	let imageBottom;
+	let windowHeight;
+
+	function calculate() {
+		imageTop = mainContainer.offset().top
+		imageBottom = imageTop + mainContainer.height()
+		windowHeight = windo.height()
+	}
+
+	function handleScroll(ypos) {
+		// console.log(imageTop < ypos + windowHeight)
+		if (ypos - 500 < imageBottom && imageTop - 500 < ypos + windowHeight) {
+			publisher.unsubscribe('WindowScrolled', handleScroll)
+			publisher.unsubscribe('Calculate', calculate);
+			loadFullImage()
+		}
+	}
+
 	function loadFullImage() {
 		const src = fullImage.attr('data-full-src');
 		const srcset = fullImage.attr('data-full-srcset');
 		fullImage.attr('src', src)
-			.attr('srcset', srcset)
-			.removeAttr('data-full-src')
-			.removeAttr('data-full-srcset');
+			.attr('srcset', srcset);
+			// .removeAttr('data-full-src')
+			// .removeAttr('data-full-srcset');
 	}
 
-	publisher.subscribe('windowLoaded', loadFullImage);
+	if (mainContainer.attr('data-lazy') === 'true') {
+		calculate()
+		handleScroll(windo.scrollTop())
+		publisher.subscribe('WindowScrolled', handleScroll);
+		publisher.subscribe('Calculate', calculate);
+
+	} else {
+		publisher.subscribe('windowLoaded', loadFullImage);
+	}
 }
 
 function watchImages() {
