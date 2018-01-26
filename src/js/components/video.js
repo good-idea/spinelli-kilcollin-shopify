83fbcf17b651a94b7eee7c4684b10a2e95a7000c
Joseph Thomas <info@standard-quality.biz>
2018-01-26 13:54:56 -0800

    Adds video sources

--- src/js/components/video.js
+++ src/js/components/video.js
@@ -3,22 +3,29 @@ import $ from 'npm-zepto'
 const buildVideo = (element, publisher) => {
 	const container = element instanceof $ ? element : $(element)
 	const video = container.find('video')[0]
+	// const $video = $(video)
 
 	const show = () => setTimeout(() => container.addClass('visible'), 300)
 	const hide = () => container.removeClass('visible')
 
-	const setLazySrc = () => {
-		const src = video.getAttribute('data-src')
-		video.setAttribute('src', src)
-	}
+	// const setLazySrc = () => {
+	// 	const sources = $video.find('source')
+	// 	sources.map((i, source) => {
+	// 		console.log(source)
+	// 		// const src = source.getAttribute('data-src')
+	// 		// source.setAttribute('src', src)
+	// 	})
+	// 	console.log($video.attr('autoplay'))
+	// 	if ($video.attr('autoplay')) video.play()
+	// }
 
 	video.addEventListener('playing', show)
 	video.addEventListener('error', hide)
 	if (!video.paused) show()
 
-	if (video.getAttribute('data-lazy') === 'true') {
-		publisher.subscribe('windowLoaded', setLazySrc)
-	}
+	// if (video.getAttribute('data-lazy') === 'true') {
+	// 	publisher.subscribe('windowLoaded', setLazySrc)
+	// }
 }
 
 const buildVideos = publisher => {
