8012b5613cb7de0527b997ab6dfd3488e8f52360
Joseph Thomas <joseph@good-idea.studio>
2018-04-23 14:11:07 -0700

    Removes captcha alert callback

--- src/js/components/handleRegistrationCaptcha.js
+++ src/js/components/handleRegistrationCaptcha.js
@@ -4,9 +4,6 @@ const handleCaptcha = e => {
 	e.preventDefault()
 	const form = $('form#create_customer')
 	const inputs = form.find('input')
-	console.log(inputs)
-	alert('ok!')
-	debugger
 }
 
 window.handleRegistrationCaptcha = handleCaptcha
