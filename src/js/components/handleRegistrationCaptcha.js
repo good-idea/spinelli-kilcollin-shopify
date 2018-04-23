d5bc2c5a28857238ed58924544ca422e1e3a82ac
Joseph Thomas <joseph@good-idea.studio>
2018-04-23 15:44:46 -0700

    Improve invisible captcha

--- src/js/components/handleRegistrationCaptcha.js
+++ src/js/components/handleRegistrationCaptcha.js
@@ -1,9 +1,25 @@
 import $ from 'jquery'
 
-const handleCaptcha = e => {
-	e.preventDefault()
-	const form = $('form#create_customer')
-	const inputs = form.find('input')
+const form = $('form#create_customer')
+const isBotField = form.find('.form__is-bot-field')
+const inputs = form.find('input')
+
+let verified = false
+
+const handleCaptcha = () => {
+	isBotField.remove()
+	for (const input of inputs) {
+		const name = input.getAttribute('name').replace(/unverified-(.*)/, '$1')
+		input.setAttribute('name', name)
+	}
+	$('#g-recaptcha-response').remove()
+	verified = true
+	form.submit()
 }
+
+form.on('submit', e => {
+	console.log('submit?')
+	if (!verified) e.preventDefault()
+})
 
 window.handleRegistrationCaptcha = handleCaptcha
