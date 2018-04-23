a897dd782f4d651b03892117bda171ffd2735092
Joseph Thomas <joseph@good-idea.studio>
2018-04-23 14:06:35 -0700

    Adds invisible captcha

--- src/js/components/handleRegistrationCaptcha.js
+++ src/js/components/handleRegistrationCaptcha.js
@@ -1,9 +1,11 @@
 import $ from 'jquery'
 
-const handleCaptcha = () => {
+const handleCaptcha = e => {
+	e.preventDefault()
 	const form = $('form#create_customer')
 	const inputs = form.find('input')
 	console.log(inputs)
+	alert('ok!')
 	debugger
 }
 
