effc8772f64329698a2e77dbbb12d62af5b38a96
Joseph Thomas <joseph@good-idea.studio>
2018-08-31 15:52:50 -0700

    Adds unmute, updates video quality settings

--- src/js/components/video.js
+++ src/js/components/video.js
@@ -1,17 +1,85 @@
 import $ from 'npm-zepto'
 
-const buildVideo = element => {
+const windo = $(window)
+
+const buildVideo = (element, publisher) => {
 	const container = element instanceof $ ? element : $(element)
 	const video = container.find('video')[0]
+
+	const btn = $("<button type='button' class='video__unmute'>Mute</button>")
 	// const $video = $(video)
+	let videoTop
+	let videoBottom
+	let windowHeight
+
+	function calculate() {
+		videoTop = container.offset().top
+		videoBottom = videoTop + container.height()
+		windowHeight = windo.height()
+	}
 
 	const createUnmuteButton = () => {
-		const btn = $("<button type='button' class='video__unmute'>Unmute</button")
 		container.append(btn)
 	}
 
-	const show = () => setTimeout(() => container.addClass('visible'), 300)
-	const hide = () => container.removeClass('visible')
+	const updateMuteButton = () => {
+		if (video.volume === 0) {
+			btn.text('Unmute')
+		} else {
+			btn.text('Mute')
+		}
+	}
+
+	const handleMuteButton = () => {
+		if (video.volume === 0) {
+			video.volume = 1
+			updateMuteButton()
+		} else {
+			video.volume = 0
+			updateMuteButton()
+		}
+	}
+
+	const handlePlay = () => {
+		updateMuteButton()
+		setTimeout(() => container.addClass('visible'), 300)
+	}
+
+	const handleError = () => {
+		container.removeClass('visible')
+	}
+
+	const loadVideo = () => {
+		const sources = container.find('source')
+		sources.each((i, source) => {
+			const src = source.getAttribute('data-full-src')
+			if (src) {
+				source.setAttribute('src', src)
+			}
+		})
+	}
+
+	function handleScroll(ypos) {
+		// console.log(videoTop < ypos + windowHeight)
+		if (ypos - 500 < videoBottom && videoTop - 500 < ypos + windowHeight) {
+			publisher.unsubscribe('WindowScrolled', handleScroll)
+			publisher.unsubscribe('Calculate', calculate)
+			loadVideo()
+		}
+	}
+
+	video.addEventListener('load', () => {
+		console.log('video loaded')
+	})
+
+	if (video.getAttribute('data-lazy') === 'true') {
+		calculate()
+		handleScroll(windo.scrollTop())
+		publisher.subscribe('WindowScrolled', handleScroll)
+		publisher.subscribe('Calculate', calculate)
+	} else {
+		publisher.subscribe('windowLoaded', loadVideo)
+	}
 
 	// const setLazySrc = () => {
 	// 	const sources = $video.find('source')
@@ -25,9 +93,10 @@ const buildVideo = element => {
 	// }
 	createUnmuteButton()
 
-	video.addEventListener('playing', show)
-	video.addEventListener('error', hide)
-	if (!video.paused) show()
+	video.addEventListener('playing', handlePlay)
+	video.addEventListener('error', handleError)
+	btn.on('click', handleMuteButton)
+	if (!video.paused) handlePlay()
 
 	// if (video.getAttribute('data-lazy') === 'true') {
 	// 	publisher.subscribe('windowLoaded', setLazySrc)
